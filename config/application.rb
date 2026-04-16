a28592579b6f15283b4bbef82a382003a85eba21
Nicolas Goarant <>
2026-04-16 11:30:19 +0200

    fix: suppression doublon httparty + email confirmation fr + locale fr

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Lotys
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.middleware.use Rack::Attack
+    config.i18n.default_locale = :fr
     config.active_job.queue_adapter = :good_job
 
   end
